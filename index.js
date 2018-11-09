018708424ca6ca84af28d7818d56868be597873c
Lee Len Formento <leebatef@gmail.com>
2018-11-09 00:38:52 -0500

    GET for actions and posts

--- index.js
+++ index.js
@@ -2,18 +2,40 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const actionDb = require('./data/helpers/actionModel');
+const projectDb = require('./data/helpers/projectModel');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet()); 
-server.use(morgan()); 
+server.use(morgan('tiny')); 
 
+server.get('/', (req, res) => {
+    res.json('Home');
+  });
+  
 // ACTION MODEL CRUD
-
-
+server.get('/api/actions', (req, res) => {
+    actionDb.get()
+      .then(actions => {
+        console.log('\n*** actions ***', actions);
+        res.status(200).json(actions);
+      })
+        .catch(err => res.status(500).json({ error: "The actions could not be retrieved. "}))
+    })
 
 
 // PROJECT MODEL CRUD
+server.get('/api/projects', (req, res) => {
+    projectDb.get()
+      .then(projects => {
+        console.log('\n*** projects ***', projects);
+        res.status(200).json(projects);
+      })
+      .catch(err => res.status(500).json({ error: "The projects could not be retrieved. "}))
+    })
+
 
 
 
