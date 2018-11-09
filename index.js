77e6f2640f997f882829d51e54474d8a178587a5
Lee Len Formento <leebatef@gmail.com>
2018-11-09 00:43:29 -0500

    GET for actions and projects by ID

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ server.get('/api/actions', (req, res) => {
         .catch(err => res.status(500).json({ error: "The actions could not be retrieved. "}))
     })
 
+server.get('/api/actions/:id', (req, res) => {
+  const { id } = req.params;
+  actionDb.get(id)
+    .then(action=> {
+    console.log('\n*** projects ***', action);
+      res.status(200).json(action);
+    })
+    .catch(err => res.status(500).json({ error: "The action with this ID not be retrieved. "}))
+})
+
+
+
 
 // PROJECT MODEL CRUD
 server.get('/api/projects', (req, res) => {
@@ -36,6 +48,15 @@ server.get('/api/projects', (req, res) => {
       .catch(err => res.status(500).json({ error: "The projects could not be retrieved. "}))
     })
 
+server.get('/api/projects/:id', (req, res) => {
+  const { id } = req.params;
+  projectDb.get(id)
+    .then(project => {
+    console.log('\n*** projects ***', project);
+      res.status(200).json(project);
+    })
+    .catch(err => res.status(500).json({ error: "The project with this ID not be retrieved. "}))
+})
 
 
 
